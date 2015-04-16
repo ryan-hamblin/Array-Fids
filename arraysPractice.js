af5288e569425ef2e0993b16d82837033a73137c
Ryan Hamblin <ryanhca@gmail.com>
2015-04-16 11:36:12 -0600

    first commit

--- arraysPractice.js
+++ arraysPractice.js
@@ -4,7 +4,10 @@
 var arr = [10,20,30];
 //Create a function named 'first' that is given 'arr' as the argument and  returns the first item the given array.
 
-  //Code Here
+var first = function(arr){
+  return arr.0;
+}
+first(arr);
 
 
 //Next problem
